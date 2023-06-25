e160ada1f8f7119830f7d9b0b691a8296744f703
bibinwithab <bibingraceson@gmail.com>
2023-06-25 12:02:32 +0530

    Documented weather.sh

--- weather.sh
+++ weather.sh
@@ -1,8 +1,18 @@
 #!/bin/bash/
 
+# This script reads a list of cities from a file called cities.txt and uses the wttr.in API to get the weather for each city.
+
+# Loop through each city in the cities.txt file
+
 for city in $(cat cities.txt);
 do
-	weather=$(curl -s http://wttr.in/$city?format=3)
-	echo "The weather for $weather"
 
+	# Use curl to get the weather for the current city from the wttr.in API
+
+	weather=$(curl -s http://wttr.in/$city?format=3)
+
+
+	# Print the weather for the current city
+
+	echo "The weather for $city is $weather"
 done
